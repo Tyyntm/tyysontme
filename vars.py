673740825362eeccc2b5522a821e0c3bbb46bc31
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-03-08 05:41:25 +0000

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,34 @@
+api_id, api_hash, bot_token,  
+pics, nor_img, welcom_vid, spell_img, admins,
+auth_group, auth_users, support_chat_id, 
+database_uri, database_name, auth_channel
+
+# Bot information
+api_id = ""
+api_hash = ""
+bot_token = ""
+
+# Bot images & videos
+pics = ''
+nor_img = ''
+welcom_vid = ''
+spell_img = ''
+
+# Admins, Channels & Users
+admins = ''
+channels = '' # file channel id @IDFinder_Robot
+
+auth_users = ''
+auth_group = ''
+
+support_chat_id = ''
+reqst_channel_id = '' # use request
+
+# MongoDB information
+database_uri = ''
+database_name = ''
+
+# fsub
+auth_channel = ''
+req_channel = ''
+join_reqs_db = ''
